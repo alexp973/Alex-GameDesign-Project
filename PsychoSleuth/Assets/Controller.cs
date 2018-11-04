e2757667753a5f74d8d8b116e96fed9d58c096b7
DeepakGohil95 <deepak.gohil1995@gmail.com>
2018-11-04 19:09:50 +0000

    First Commit
    Added Unity Project to GitHub

---
+++ PsychoSleuth/Assets/Controller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	Vector3 targetPosition;
+	Vector3 lookAtTarget;
+	Quaternion playerRot;
+	float rotSpeed = 5;
+	float speed = 10;
+	bool moving = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0))
+		{
+			SetTargetPosition();
+		}
+
+		if(moving)
+			Move();
+		
+	}
+
+	void SetTargetPosition()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, 1000))
+		{
+			targetPosition = hit.point;
+			//this.transform.LookAt(targetPosition);
+			lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
+				transform.position.y,
+				targetPosition.z - transform.position.z);
+			playerRot = Quaternion.LookRotation(lookAtTarget);
+			moving = true;
+		}
+
+	}
+
+	void Move()
+	{
+		transform.rotation = Quaternion.Slerp(transform.rotation,
+													playerRot,
+													rotSpeed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards(transform.position,
+													targetPosition,
+													speed * Time.deltaTime);
+
+		if(transform.position == targetPosition)
+			moving = false;											
+
+
+	}
+
+}
